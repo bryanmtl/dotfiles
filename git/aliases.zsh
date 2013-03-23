4635757ce02d0ee0532dcf02c69bc9ed8a3c2a01
bryanmtl <bryan@godynamo.com>
2013-03-23 09:45:12 -0400

    Updates to most recent version

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,13 +6,27 @@ then
   alias git=$hub_path
 fi
 
+function current_branch() {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo ${ref#refs/heads/}
+}
+
 # The rest of my fun git aliases
+alias g='git'
+alias gst='git status'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
-alias gca='git commit -a'
+alias gca='git commit -v -a'
+alias gba='git branch -a'
+alias gcp='git cherry-pick'
+alias ga='git add'
+alias gm='git merge'
+alias grhh='git reset HEAD --hard'
+alias ggpull='git pull --rebase origin $(current_branch)'
+alias ggpush='git push origin $(current_branch)'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
